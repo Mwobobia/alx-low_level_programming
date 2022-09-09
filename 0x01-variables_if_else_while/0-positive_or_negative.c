1a00aa51451b07487d3a2662dbd5d27b65251f42
Mwobobia <mwobobiacaroline@gmail.com>
2022-09-09 15:27:04 -0700

    0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,16 +1,20 @@
-#include <stdio.h>
+#include <stdio.h>
 
 #include <stdlib.h>
 
 #include <time.h>
 
 /**
+ *
+ *  * main - Entry point
+ *
+ *   *
+ *
+ *    * Return: Always 0 (Seccess/correct)
+ *
+ *     */
 
- * main - Entry point
 
- * Return: Always 0 (Success)
-
- */
 
 int main(void)
 
@@ -22,25 +26,30 @@ int main(void)
 
 	n = rand() - RAND_MAX / 2;
 
-	if (n > 0)
+	if (n == 0)
 
-		printf("%d is positive\n", n);
-
-	else if (n == 0)
+	{
 
 		printf("%d is zero\n", n);
 
-	else
+	}
+
+	else if (n < 0)
+
+	{
 
 		printf("%d is negative\n", n);
+
+	}
+
+	else
+
+	{
+
+		printf("%d is positive\n", n);
+
+	}
 
 	return (0);
 
 }
-
-^C
-^c
-clear
-root@1b25d9e085cf:/alx-low_level_programming/0x01-variables_if_else_while# ls  
-
-root@1b25d9e085cf:/alx-low_level_programming/0x01-variables_if_else_while#
