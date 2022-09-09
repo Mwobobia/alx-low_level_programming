6606cceb40dc7a76f4926b556963a96f3f7eeacd
Mwobobia <mwobobiacaroline@gmail.com>
2022-09-09 15:46:09 -0700

    1-last_digit.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -4,15 +4,15 @@
 
 #include <time.h>
 
-/**
+/*
  *
- *  * main - Entry point
+ * main - Entry point
  *
- *   *
+ * 
  *
- *    * Return: Always 0 (Seccess/correct)
+ * Return: Always 0 (Seccess/correct)
  *
- *     */
+ */
 
 
 
