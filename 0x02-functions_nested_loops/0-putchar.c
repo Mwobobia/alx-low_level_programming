350ff45aca3d08abb57516de658ca00f9107a42f
Mwobobia <mwobobiacaroline@gmail.com>
2022-09-14 06:15:39 -0700

    README.md

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,4 @@
-#include <main.h>
+#include "main.h"
 
 /**
  * main - prints _putchar, followed by a new line
@@ -6,14 +6,14 @@
  */
 int main(void)
 {
-	_putchar('p')
-	_putchar('u')
-	_putchar('t')
-	_putchar('c')
-	_putchar('h')
-	_putchar('a')
-	_putchar('r')
-	_putchar('\n')
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 
 	return (0);
 }
