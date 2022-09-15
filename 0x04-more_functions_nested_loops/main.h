0774b92898648f64706054b72d0a1a7f622facb5
mwobobia <mwobobiacaroline@gmail.com>
2022-09-15 19:15:46 +0300

    main edit

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -7,6 +7,7 @@ int _isdigit(int c);
 int mul(int a, int b);
 void print_numbers(void);
 void print_line(int n);
+void print_diagonal(int n);
 void positive_or_negative(int i);
 void print_most_numbers(void);
 void more_numbers(void);
