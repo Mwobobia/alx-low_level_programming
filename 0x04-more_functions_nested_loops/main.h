3bfbfe3c964ed2a2825d91d555d8c18d5a418ece
mwobobia <mwobobiacaroline@gmail.com>
2022-09-16 13:03:47 +0300

    prototypes

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -10,15 +10,6 @@ void print_line(int n);
 void print_diagonal(int n);
 void print_square(int size);
 void print_triangle(int size);
-void positive_or_negative(int i);
-void print_most_numbers(void);
-void more_numbers(void);
-int largest_number(int, int, int);
-
-void print_remaining_days(int, int, int);
-
-int convert_day(int, int);
-
-
+void print_number(int n);
 
 #endif /* MAIN.H */
