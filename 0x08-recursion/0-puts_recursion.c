431713a3dcb13e75836e2d555cdcb85a4df7bc41
user name <mwobobiacaroline@gmail.com>
2022-09-28 18:12:23 +0300

    used to test your functions

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,13 +1,14 @@
 #include "main.h"
+#include <string.h>
 
 /**
- * _puts_recursion - prototype to print string
- * @s: string to be printed
+ * _puts_recursion - prototype to print string.
+ * @s: parameter
  */
 
 void _puts_recursion(char *s)
 {
-	if (*s)
+	if (*s != '\0')
 	{
 		_putchar(*s);
 		_puts_recursion(s + 1);
