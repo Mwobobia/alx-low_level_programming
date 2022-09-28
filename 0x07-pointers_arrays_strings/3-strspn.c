c8f53293e9ff00d1a8ee82856c366df276acf044
mwobobia <mwobobiacaroline@gmail.com>
2022-09-28 13:13:33 +0300

    a function that locates a substring

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -2,34 +2,23 @@
 
 /**
  * *_strspn - gets the length of a prefix substring
- * @s: string to evaluate
- * @accept: string containing the list of characters to match in s
- *
- * Return: the number of of bytes in the initial segment
- * of s which consist only of bytes from accept
+ * @s: string to check
+ * @accept: string to check
+ * Return: length
  */
 unsigned int _strspn(char *s, char *accept)
 {
-	int i, j, f, flag;
+	unsigned int i, j, count = 0;
 
-	f = 0;
-
-	for (i = 0; s[i] != '\0'; i++)
+	for (i = 0; s[i] != ' ' && s[i] != '\0'; i++)
 	{
-		flag = 0;
 		for (j = 0; accept[j] != '\0'; j++)
 		{
 				if (s[i] == accept[j])
 				{
-				f++;
-				flag = 1;
+					count++;
 				}
 		}
-		if (flag == 0)
-		{
-			return (f);
-		}
 	}
-
-	return (0);
+	return (count);
 }
