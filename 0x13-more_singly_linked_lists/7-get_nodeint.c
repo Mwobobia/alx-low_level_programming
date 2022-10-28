35c6553b231a0bcc7f268e9d14bb168ac648777e
Mwobobia <mwobobiacaroline@gmail.com>
2022-10-28 17:12:04 +0300

    a function that returns the nth node of a listint_t linked list. - else correction

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -1,31 +1,23 @@
 #include "lists.h"
+
 /**
- * get_nodeint_at_index - function that return an index of node
- * @head: the head of the linked list
- * @index: the index we want to remove
- * Return: the node from the index
+ * get_nodeint_at_index - returns the nth node of a listint_t linked list.
+ * @head: pointer to the first node in the list
+ * @index: index of the node to return
+ *
+ * Return: pointer to the indexed node
  */
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
-	unsigned int i = 0;
+	unsigned int i;
 
-	listint_t *tm;
-
-	if (!head)
-		return (0);
-	for
+	if (head == NULL)
+		return (NULL);
+	for (i = 0; i < index; i++)
 	{
-		if (index == 0)
-			return (head);
-		i++;
-		tm = head->next;
-		while (tm)
-		{
-			if (index == i)
-				return (tm);
-			tm = tm->next;
-			i++;
-		}
+		head = head->next;
+		if (head == NULL)
+			return (NULL);
 	}
-	return (NULL);
+	return (head);
 }
