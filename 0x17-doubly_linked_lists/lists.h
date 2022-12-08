dfaae9cd1a81e8dbaa433672023e68c473d90040
Mwobobia <Mwobobia>
2022-12-08 16:12:39 +0300

    lists.h

--- 0x17-doubly_linked_lists/lists.h
+++ 0x17-doubly_linked_lists/lists.h
@@ -13,13 +13,13 @@
  * Description: doubly linked list node structure
  */
 
-typedef struct dlistint_s
+struct dlistint_s
 {
     int n;
     struct dlistint_s *prev;
     struct dlistint_s *next;
-} dlistint_t;
-
+};
+typedef struct dlistint_s dlistint_t;
 size_t print_dlistint(const dlistint_t *h);
 size_t dlistint_len(const dlistint_t *h);
 dlistint_t *add_dnodeint(dlistint_t **head, const int n);
